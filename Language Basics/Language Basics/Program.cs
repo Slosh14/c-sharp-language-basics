705221386a5a49de40c1068398dbf301986b5db3
Slosh14 <leggrjames@outlook.com>
2021-10-15 14:02:45 +1100

    basic variables
    added int and string variables

--- Language Basics/Language Basics/Program.cs
+++ Language Basics/Language Basics/Program.cs
@@ -6,7 +6,11 @@ namespace Language_Basics
     {
         static void Main(string[] args)
         {
+            int age = 30;
+            string name = "James";
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is " + name + " and my age is " + age);
 
             Console.ReadLine();
         }
